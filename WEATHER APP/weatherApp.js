683723b5e42a9b8c35bd77737cd08d85a65167fa
Guru Prasad <guru96392@gmail.com>
2018-11-14 23:37:33 +0530

    different way of implementing ajax

--- WEATHER APP/weatherApp.js
+++ WEATHER APP/weatherApp.js
@@ -9,20 +9,36 @@ $(document).ready(function(){
 
            let url ='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=ab5762383de7f3971fc6dd9d42c9967a';
 
-           $.getJSON(url,function(data){
+        //    $.getJSON(url,function(data){
         
-	       let city = data.name;
-	       let kelvin = data.main.temp;
-	       let country = data.sys.country;
-	       let temp = Math.round(kelvin-273.15);
-	       let climate = data.weather[0].description;
+         // let city = data.name;
+         // let kelvin = data.main.temp;
+         // let country = data.sys.country;
+         // let temp = Math.round(kelvin-273.15);
+         // let climate = data.weather[0].description;
 
-           document.getElementById("city").innerHTML =city + "," + country;
-           document.getElementById("temp").innerHTML = temp +"&deg;C";
-           document.getElementById("climate").innerHTML = climate;
+        //    document.getElementById("city").innerHTML =city + "," + country;
+        //    document.getElementById("temp").innerHTML = temp +"&deg;C";
+        //    document.getElementById("climate").innerHTML = climate;
            
-           });
+        //          
 
+           $.ajax(url,{
+            dataType:"json",
+            method:"GET",
+
+            success:function(data){
+                 let city = data.name;
+                 let kelvin = data.main.temp;
+                 let country = data.sys.country;
+                 let temp = Math.round(kelvin-273.15);
+                 let climate = data.weather[0].description;
+
+                 document.getElementById("city").innerHTML =city + "," + country;
+                 document.getElementById("temp").innerHTML = temp +"&deg;C";
+                 document.getElementById("climate").innerHTML = climate;
+            }
+           });
       });
 
   });
@@ -30,5 +46,5 @@ $(document).ready(function(){
 }
 
 else{
-  	console.log("Not able to load the weather information");
+    console.log("Not able to load the weather information");
   }
